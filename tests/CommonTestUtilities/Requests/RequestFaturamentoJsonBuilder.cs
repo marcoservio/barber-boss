c1951cdee8b8a1ed229c5b507fff3ecbe0d76889
Marco Servio Almeida Capanema <marco.capanema@lg.com.br>
2025-06-16 11:25:33 -0300

    test: adding unit tests from validation

---
+++ tests/CommonTestUtilities/Requests/RequestFaturamentoJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using BarberBoss.Communication.Enums;
+using BarberBoss.Communication.Requests;
+using Bogus;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestFaturamentoJsonBuilder
+{
+    public static RequestFaturamentoJson Build()
+    {
+        return new Faker<RequestFaturamentoJson>()
+            .RuleFor(r => r.Title, f => f.Commerce.ProductName())
+            .RuleFor(r => r.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, f => f.Date.Past())
+            .RuleFor(r => r.PaymentType, f => f.PickRandom<PaymentType>())
+            .RuleFor(r => r.Amount, f => f.Random.Decimal(min: 1, max: 1000));
+    }
+}
---
+++ tests/Validators.Tests/Faturamento/FaturamentoValidationTest.cs
@@ -0,0 +1,70 @@
+﻿using BarberBoss.Application.UseCases.Faturamento;
+using BarberBoss.Communication.Enums;
+using BarberBoss.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Faturamento;
+
+public class FaturamentoValidationTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new FaturamentoValidator();
+        var request = RequestFaturamentoJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("     ")]
+    [InlineData(null)]
+    public void Error_Title_Empty(string title)
+    {
+        var validator = new FaturamentoValidator();
+        var request = RequestFaturamentoJsonBuilder.Build();
+        request.Title = title;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+    }
+
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        var validator = new FaturamentoValidator();
+        var request = RequestFaturamentoJsonBuilder.Build();
+        request.PaymentType = (PaymentType)1000;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-7)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        var validator = new FaturamentoValidator();
+        var request = RequestFaturamentoJsonBuilder.Build();
+        request.Amount = amount;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+    }
+}
